98ded8aedfe5ee0661f8b8d9ac7de7916db7a628
Gayathri <gayathrisudi2004@gmail.com>
2026-01-18 14:45:32 +0530

    Ignore IntelliJ IDEA files and application.properties

--- backend/src/main/java/com/codencode/foody/FoodyApplication.java
+++ backend/src/main/java/com/codencode/foody/FoodyApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FoodyApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(FoodyApplication.class, args);
 	}
 
